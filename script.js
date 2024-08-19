c180a6a1c21da81385d8aaeeaa1cca6f0822b050
Kerlon Ribeiro <124305955+kerlonr@users.noreply.github.com>
2024-08-18 23:55:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (event) {
-    // Impede o envio padrão do formulário para que possamos limpar os campos primeiro
     event.preventDefault();
 
-    // Aqui você pode fazer qualquer ação adicional antes de enviar, se necessário
-
-    // Limpa os campos do formulário
     this.reset();
 
-    // Envia o formulário após limpar
     this.submit();
   });
 
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
-const icons = document.querySelector(".icons"); // Seleciona os ícones
+const icons = document.querySelector(".icons");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
-  icons.classList.toggle("active"); // Alterna a visibilidade dos ícones
+  icons.classList.toggle("active");
 });
